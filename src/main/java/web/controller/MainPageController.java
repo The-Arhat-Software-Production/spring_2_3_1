2ca62d84a331016a3e72e8a3d44c18d57a4c5276
Sergei Akopov <arhat23@gmail.com>
2024-01-19 10:44:06 +0300

    updated some files

--- src/main/java/web/controller/MainPageController.java
+++ src/main/java/web/controller/MainPageController.java
@@ -11,17 +11,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class MainPageController {
-    @GetMapping(value="", produces = "text/html; charset=utf-8")
+    @GetMapping("")
     public String mainPageView(Model model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Написать CRUD-приложение. Использовать Spring MVC + Hibernate.");
-        messages.add("Должен быть класс {{User }}с произвольными полями (id, name и т.п.).");
-        messages.add("В приложении должна быть страница, на которую выводятся все юзеры с возможностью добавлять," +
-                " удалять и изменять юзера.");
-        messages.add("Конфигурация Spring через {{JavaConfig }}и аннотации, по аналогии с предыдущими проектами. " +
-                "Без использования xml. Без Spring Boot.");
-        messages.add("Внесите изменения в конфигурацию для работы с базой данных. Вместо SessionFactory должен использоваться EntityManager.");
-        model.addAttribute("messages", messages);
         return "index";
     }
 }
